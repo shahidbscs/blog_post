59e8df2dfd3d4da911807d18e25886d0f4080ad2
shahidbscs <shahidbscs@gmail.com>
2016-08-17 15:03:33 +0400

    Add comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,6 +6,15 @@ class CommentsController < ApplicationController
     redirect_to blog_path(@blog)
   end
 
+  def destroy
+    @blog = Blog.find(params[:blog_id])
+    @comment = @blog.comments.find(params[:id])
+
+    @comment.destroy
+
+    redirect_to @blog
+  end
+
   private
     def comment_params
       params.require(:comment).permit(:subject, :body)
